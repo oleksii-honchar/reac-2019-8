185797c6425a5cabe8f01c4dc9b74bb1f6b8e9f9
olho <olho@ciklum.com>
2019-08-23 11:44:39 +0300

    + muicss

--- src/index.js
+++ src/index.js
@@ -9,27 +9,26 @@ import { Root } from './modules/Root';
 
 window.pkg = JSON.parse(atob(PKG));
 window.name = window.pkg.name;
+window.config = window.config || {
+  logLevel: 'info',
+  isNode: false
+};
+
 LoggerService.setGlobalTitle(window.pkg.name);
 
 const logger = new LoggerService();
 logger.setTitle('index');
 
-window.config = window.config || {};
-window.config.isNode = false;
 
 // const axiosHttpAdapter = new AxiosHttpAdapter()
 // export const rootApiSvc = new ApiService(axiosHttpAdapter, 'api')
 
 function startApp () {
   logger.info('Starting app...');
-
   // const render = window.config.ssr ? ReactDOM.hydrate : ReactDOM.render
   //
 
-  ReactDOM.render(
-    <Root title="Hello world!" />,
-    document.querySelector('#app-root'),
-  );
+  ReactDOM.render(<Root/>, document.querySelector('#app-root'));
 }
 
 // if (window.config.startApp) startApp();
